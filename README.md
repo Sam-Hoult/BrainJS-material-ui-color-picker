c793c8acce4bcc58c63973671c3469a3fb28208c
Sam Hoult <sam.o.hoult@gmail.com>
2018-04-01 21:37:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Brain.js Material UI machine learning color detector
+A simple utility to use a neural network to solve whether a background colour requires light or dark foreground text.
+
+## Running
+To get started, make sure you have node installed
+* Install dependencies: `npm install` or `yarn`
+* Run the demo: `npm run start`
+* Edit index.js to change the code
+* Data available from material-ui
---
+++ index.js
@@ -0,0 +1,16 @@
+const Brain = require('brain.js')
+const materialUiData = require('./materialui-color-data.js')
+const hexToRGB = require('./utility.js')
+
+// Make the neural network
+const net = new Brain.NeuralNetwork();
+
+// Feed it the data
+net.train(materialUiData);
+
+const hex = '#5683a0'
+const convertedHex = hexToRGB(hex)
+const input = { r: 1, g: 0.4, b: 0 }
+
+console.log('Output: ', Brain.likely(input, net))
+console.log('Hex output: ', Brain.likely(input, net))
